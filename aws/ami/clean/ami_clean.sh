ee0a7eab0379509a7219eaec22f0321be034fe39
Justin Harris <Justin.Harris@accenturefederal.com>
2015-12-22 11:43:05 -0500

    added script to clean ami

---
+++ aws/ami/clean/ami_clean.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -x
+/sbin/service rsyslog stop 2>/dev/null
+/sbin/service auditd stop 2>/dev/null
+/usr/bin/yum clean all
+/usr/sbin/logrotate -f /etc/logrotate.conf
+#/bin/rm -f /var/log/*-???????? /var/log/*.gz
+#/bin/rm -f /var/log/dmesg.old
+/bin/rm -rf /var/log/anaconda
+find / -name .bash_history -exec shred -zu {} \;
+find /etc -name ssh_host\* -exec shred -zu {} \;
+find / -name .ssh -print | while read i; do sudo shred -zu ${i}/* 2>/dev/null; done
+
+shred -zu /var/log/* 2>/dev/null
+shred -zu /var/log/audit/* 2>/dev/null
+/bin/cat /dev/null > /var/log/audit/audit.log
+/bin/cat /dev/null > /var/log/wtmp
+/bin/cat /dev/null > /var/log/lastlog
+
+shred -zu /etc/udev/rules.d/70*
